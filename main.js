de914ea9f7f61198c7de915f6ef4d8b699601564
tantoine04 <tantoine04@gmail.com>
2020-06-12 01:49:33 +0000

    calculator

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-let result = document.querySelector(".result");
-    let btns = document.querySelector(".buttons");
-    let ops = document.querySelectorAll(".operator");
+// let result = document.querySelector(".result");
+//     let btns = document.querySelector(".buttons");
+//     let ops = document.querySelectorAll(".operator");
     
+    // let buttons = document.querySelectorAll(".buttons")
+    // console.log(buttons);
+    // for(let button of buttons) {
+    //   buttons.addEventListener('click', function() {
+    //   )
+    //   }
+      
+    // }
+
+    function getButton () {
+      let buttons = document.querySelectorAll(".buttons");
+      console.log(buttons)
+      let display = document.querySelector(".result");
+      for (let button of buttons) {
+        console.log(button.textContent);
+        button.addEventListener("click", function () {
+          alert(button.textContent);
+        })
+      }
+    }
+
+    getButton();
+
